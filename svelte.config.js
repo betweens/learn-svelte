9058d43f40db0b184864c3889ae699a54cc50ccd
Mingfei Hu <mingfei.hu@volkswagen.com.cn>
2025-12-26 11:03:26 +0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,10 +9,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: { 
-		adapter: adapter({
-			edge: false,
-			split: false
-		}),
+		adapter: adapter(),
 		experimental: {
 			remoteFunctions: true
 		}
